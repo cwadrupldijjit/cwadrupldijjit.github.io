a8b162209f5ec320a8b00238a82993633038858d
Samuel Skeen <sskeen@sskeendevelopment.onmicrosoft.com>
2015-11-12 15:32:18 -0700

    Gulp works!

--- js/app.js
+++ js/app.js
@@ -1,2 +1,13 @@
-console.log('test');
-// Does this work 
+/// <reference path="../typings/angularjs/angular" />
+/// <reference path="../typings/angular-ui-router/angular-ui-router" />
+/// <reference path="../typings/jquery/jquery" />
+var app = angular.module('PageApp', ['ui.router', 'ngMaterial']);
+app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function ($stateProvider, $urlRouterProvider, $locationProvider) {
+        $stateProvider
+            .state('Home', {
+            url: '/',
+            templateUrl: '/views/home/home.html',
+            controller: 'HomeController'
+        });
+        $urlRouterProvider.otherwise('/');
+    }]);
--- ts/app.ts
+++ ts/app.ts
@@ -3,3 +3,14 @@
 /// <reference path="../typings/jquery/jquery" />
 
 let app = angular.module('PageApp', ['ui.router', 'ngMaterial']);
+
+app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
+	$stateProvider
+		.state('Home', {
+			url: '/',
+			templateUrl: '/views/home/home.html',
+			controller: 'HomeController'
+		});
+	
+	$urlRouterProvider.otherwise('/');
+}]);
