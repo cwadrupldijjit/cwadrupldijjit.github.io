964ce75be1dbd68fa8041dfee7904f678e79c97d
Sam Skeen <sskeen@sskeendevelopment.onmicrosoft.com>
2016-01-24 20:27:35 -0700

    Copying code from the other repo into the places that should be set up for them

--- app/subpages/ng2-parallax/components/parallax-app/parallax.app.component.ts
+++ app/subpages/ng2-parallax/components/parallax-app/parallax.app.component.ts
@@ -2,11 +2,13 @@ import { Component,
 		 View } from 'angular2/core';
 import { RouteConfig,
 		 ROUTER_DIRECTIVES } from 'angular2/router';
+import { Parallax,
+		 ParallaxConfig } from '../../directives/ng2-parallax.directive';
 import { ParallaxHome } from '../parallax-home/parallax-home.component';
 import { tsExamples } from '../ts-examples/ts-examples.component';
 
 @Component({
-	selector: 'ng2-parallax'
+	selector: 'parallax-app'
 })
 @View({
 	templateUrl: 'app/subpages/ng2-parallax/components/parallax-app/parallax.app.template.html',
@@ -14,7 +16,8 @@ import { tsExamples } from '../ts-examples/ts-examples.component';
 		'app/subpages/ng2-parallax/components/parallax-app/parallax.app.styles.css'
 	],
 	directives: [
-		ROUTER_DIRECTIVES
+		ROUTER_DIRECTIVES,
+		Parallax
 	]
 })
 @RouteConfig([
